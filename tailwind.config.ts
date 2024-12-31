0bd5b783ce9b822e0598195732c05e56c7b115b2
Bard <59461181+prove-ability@users.noreply.github.com>
2025-01-01 01:11:10 +0900

    release/250101 - 고객지원 페이지 (#4)
    * chore: update dependencies and lockfile
    
    - Added @radix-ui/react-accordion dependency to package.json.
    - Updated bun.lockb to reflect changes in dependencies.
    
    * feat: add Accordion component with animations
    
    - Introduced a new Accordion component using Radix UI for better UI interactions.
    - Implemented custom animations for accordion open/close states in Tailwind CSS.
    
    * feat: add Customer Support page with structured content
    
    - Introduced a new Customer Support page featuring a detailed education process and FAQ section.
    - Added an accordion component for FAQs to enhance user interaction and information accessibility.
    - Utilized existing UI components for consistent styling and layout.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -58,11 +58,33 @@ export default {
       },
       animation: {
         scroll: "scroll 20s linear infinite",
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
       keyframes: {
         scroll: {
-          from: { transform: "translateX(0)" },
-          to: { transform: "translateX(-100%)" },
+          from: {
+            transform: "translateX(0)",
+          },
+          to: {
+            transform: "translateX(-100%)",
+          },
+        },
+        "accordion-down": {
+          from: {
+            height: "0",
+          },
+          to: {
+            height: "var(--radix-accordion-content-height)",
+          },
+        },
+        "accordion-up": {
+          from: {
+            height: "var(--radix-accordion-content-height)",
+          },
+          to: {
+            height: "0",
+          },
         },
       },
     },
