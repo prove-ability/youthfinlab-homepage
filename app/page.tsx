129ce01bed2296ecbf1db5276c39c120933a6f5f
prove-ability <prove.ability00@gmail.com>
2024-12-31 16:33:09 +0900

    refactor: move LECTURE_LOCATION to constants file
    - Updated import statement in page.tsx to reference the new location of LECTURE_LOCATION.

--- app/page.tsx
+++ app/page.tsx
@@ -4,21 +4,7 @@ import OptimizedImage from "@/components/ui/OptimizedImage";
 import Typography from "@/components/ui/Typography";
 import Button from "@/components/ui/Button";
 import Section from "@/components/ui/Section";
-
-const LECTURE_LOCATION = [
-  "서울시 중구청",
-  "YWCA",
-  "국민대학교",
-  "청소년금융교육협의회",
-  "부경대학교",
-  "BNK부산은행",
-  "하나증권",
-  "안국약품",
-  "KEB하나은행",
-  "IBK행복나눔재단",
-  "화성시문화재단",
-  "서민금융진흥원",
-] as const;
+import { LECTURE_LOCATION } from "@/app/constants/partners";
 
 export default function Home() {
   return (
