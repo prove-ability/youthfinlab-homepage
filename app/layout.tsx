878c81e78a8d2141bf946f5bf648b2ce918a8a3a
prove-ability <prove.ability00@gmail.com>
2025-01-01 16:29:04 +0900

    feat: update favicon and add web app icons
    - Removed the existing favicon.ico file.
    - Added new favicon images in various sizes (16x16, 32x32) and formats (PNG).
    - Introduced apple-touch-icon and Android Chrome icons for enhanced mobile support.

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,46 @@ export const metadata: Metadata = {
   title: "유스핀랩",
   description:
     "금융 지식이 만드는 큰 변화, 금융교육 전문기업 유스핀랩과 함께하세요",
+  icons: {
+    icon: [
+      {
+        url: "/favicon.ico",
+        sizes: "32x32",
+      },
+      {
+        url: "/favicon-16x16.png",
+        sizes: "16x16",
+        type: "image/png",
+      },
+      {
+        url: "/favicon-32x32.png",
+        sizes: "32x32",
+        type: "image/png",
+      },
+    ],
+    apple: [
+      {
+        url: "/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+      },
+    ],
+    other: [
+      {
+        rel: "android-chrome",
+        url: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        rel: "android-chrome",
+        url: "/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  },
+  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({
